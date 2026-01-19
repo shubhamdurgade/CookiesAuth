eee9f962ca32ed4b3e538979f8fce1010b979254
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-01-19 21:22:23 +0530

    Add project files.

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookiesAuth.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ Models/UserStoreService.cs
@@ -0,0 +1,23 @@
+﻿namespace CookiesAuth.Models
+{
+    public class UserStoreService
+    {
+        private List<User> _users = new List<User>
+        {
+            new User {Id = 1 , UserName = "Saurabh" ,Password = "saurabh@123"},
+            new User {Id = 2 , UserName = "Harsh" ,Password = "harsh@123"}
+        };
+
+        public User? ValidateUser(string userName, string password)
+        {
+            return _users.FirstOrDefault(u =>
+            u.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase) &&
+            u.Password == password);
+        }
+
+        public User? GetUser(int id)
+        {
+            return _users.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
